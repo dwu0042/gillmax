3d22cedab021ceba96a0f3e887f066d9e0fe8707
dwu0042 <david.wu1@monash.edu>
2024-12-16 12:12:52 +1100

    remove vignette-specific selection

--- gillespymax/analysis.py
+++ gillespymax/analysis.py
@@ -1,6 +1,7 @@
 import h5py
 import polars as pl
 from os import PathLike
+from typing import Iterable
 
 def read_records(filename: PathLike):
 
@@ -15,11 +16,11 @@ def read_records(filename: PathLike):
 
     return dataframes
 
-def plot_history(dataframe: pl.DataFrame):
+def plot_history(dataframe: pl.DataFrame, states_to_plot: Iterable):
 
     return (
         dataframe
-        .select(list('tSEIRDTQ'))
+        .select('t', list(states_to_plot))
         .unpivot(
             index='t',
             variable_name='state',
