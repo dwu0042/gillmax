e67f1bab9777d3d92b49f5880d81a21706014217
davidjxwu <david.jx.wu@gmail.com>
2025-03-13 03:57:54 +1100

    Add in a delay distribution func

--- gillespymax/analysis.py
+++ gillespymax/analysis.py
@@ -5,7 +5,7 @@ from os import PathLike
 from typing import Iterable
 
 def read_records(filename: PathLike):
-
+    """Returns a Mapping of simulation ids to parsed simulation dataframes"""
     dataframes = dict()
     with h5py.File(filename, "r") as fp:
         for group in fp:
@@ -17,7 +17,7 @@ def read_records(filename: PathLike):
 
     return dataframes
 
-def df_longer(dataframe: pl.DataFrame, states_to_plot: Iterable):
+def _df_longer(dataframe: pl.DataFrame, states_to_plot: Iterable):
     return (
         dataframe
         .select('t', *list(states_to_plot))
@@ -40,5 +40,30 @@ def _plot_hist(df_long: pl.DataFrame, ax=None, **kwargs):
     )
 
 def plot_history(dataframe: pl.DataFrame, states_to_plot: Iterable, ax=None, **kwargs):
+    """Plots the state history for the given states to plot"""
 
-    return _plot_hist(df_longer(dataframe, states_to_plot), ax=ax, **kwargs)
+    return _plot_hist(_df_longer(dataframe, states_to_plot), ax=ax, **kwargs)
+
+def compute_delay_distribution(dataframe: pl.DataFrame, init_code, completion_code):
+    """Computes the time delay between two types of events, where continuity is provided by being the same individual (enode)"""
+
+    init_enodes = dataframe.filter(pl.col('txncode').eq(init_code)).select('enode').to_series()
+    complete_enodes = dataframe.filter(pl.col('txncode').eq(completion_code)).select('enode').to_series()
+
+    return (
+        dataframe
+        .filter(
+            pl.col('enode').is_in(init_enodes),
+            pl.col('enode').is_in(complete_enodes),
+        )
+        .select(
+            't', 'enode', 'txncode',
+        )
+        .pivot(index='enode', on='txncode')
+        .select(
+            'enode',
+            (
+                pl.col(completion_code) - pl.col(init_code)
+            ).alias('delay')
+        )
+    )
